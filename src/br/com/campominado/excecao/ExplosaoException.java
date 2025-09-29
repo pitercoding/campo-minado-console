a583a8e4ad2214cb3c11d0fa7eda853e8f0b6e42
pitercoding <piterg.bio@gmail.com>
2025-09-29 13:39:54 +0200

    feat(campo): adicionar métodos de marcação, abertura e verificação de vizinhança
    - Criado método alternarMarcacao() para permitir marcar/desmarcar campos fechados
    - Criado método abrir() que:
      - abre o campo se não estiver marcado
      - lança ExplosaoException em caso de mina
      - expande automaticamente vizinhos seguros
    - Criado método vizinhancaSegura() que verifica se nenhum vizinho contém mina

---
+++ src/br/com/campominado/excecao/ExplosaoException.java
@@ -0,0 +1,5 @@
+package br.com.campominado.excecao;
+
+public class ExplosaoException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
--- src/br/com/campominado/modelo/Campo.java
+++ src/br/com/campominado/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.campominado.modelo;
 
+import br.com.campominado.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,5 +41,32 @@ public class Campo {
         } else {
             return false;
         }
+    }
+
+    void alternarMarcacao() {
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir(){
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                throw new ExplosaoException();
+            }
+
+            if (vizinhancaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean vizinhancaSegura() {
+        return  vizinhos.stream().noneMatch(v -> v.minado);
     }
 }
